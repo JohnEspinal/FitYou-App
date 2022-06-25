2afd1a1722da6d78453df6dcd5a86fe4972d0dcd
victor Jose Calderon <victorjosecaderon@victors-MacBook-Air-3.local>
2022-06-25 15:21:24 -0400

    make it better

--- src/app/plans/pages/company/company.component.ts
+++ src/app/plans/pages/company/company.component.ts
@@ -6,36 +6,41 @@ import { AuthService } from '../../../auth/services/auth.service';
 @Component({
   selector: 'app-company',
   templateUrl: './company.component.html',
-  styleUrls: ['./company.component.css']
+  styleUrls: ['./company.component.css'],
 })
 export class CompanyComponent implements OnInit {
-
-  Company!: Company[];
+  Company: any = [
+    {
+      Name: 'Altice',
+    },
+    {
+      Name: 'Claro',
+    },
+    {
+      Name: 'Viva',
+    },
+  ];
 
   isUserLoggedIn!: boolean;
 
   constructor(
-    private planservice : PlanService,
-    private authService : AuthService
-  ) { }
+    private planservice: PlanService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
-    this.planservice.getCompany().subscribe( response => {
+    this.planservice.getCompany().subscribe((response) => {
       this.Company = response;
-    } );
+    });
 
-    this.authService.validateToken()
-      .subscribe(
-        resp => this.isUserLoggedIn = resp
-      )
-
-    
+    this.authService
+      .validateToken()
+      .subscribe((resp) => (this.isUserLoggedIn = resp));
   }
 
-  eliminar(id : number){
-    this.planservice.deleteCompany(id).subscribe(Response => {
+  eliminar(id: number) {
+    this.planservice.deleteCompany(id).subscribe((Response) => {
       console.log(Response);
-    })
+    });
   }
-
 }
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -6,109 +6,98 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styles: []
+  styles: [],
 })
 export class NavbarComponent implements OnInit {
-
-    userLoggedIn: boolean = false;
+  userLoggedIn: boolean = false;
 
   items: MenuItem[] = [];
 
-  constructor(  private authService: AuthService,
-                private router: Router ) { }
+  constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {
-      
-        this.authService.validateToken()
-            .subscribe(
-                resp => {
-                    console.log("Im the RESPONSE:", resp);
-                    this.userLoggedIn = resp
+    this.authService.validateToken().subscribe((resp) => {
+      console.log('Im the RESPONSE:', resp);
+      this.userLoggedIn = resp;
 
-                    this.items = [
-                        {
-                            label: 'Inicio',
-                            icon: 'pi pi-home',
-                            routerLink: '/'
-                        },
-                        {
-                            label: 'Planes',
-                            icon: 'pi pi-comments',
-                            items: [
-                                {
-                                    label: 'Ver planes',
-                                    icon: 'pi pi-bars',
-                                    routerLink: '/plans/all'
-                                },
-                                ...( this.userLoggedIn ? [{
-                                    label: 'Agregar',
-                                    icon: 'pi pi-plus',
-                                    routerLink: '/plans/add'
-                                }]: [])
-                            ]
-                        },
-                        {
-                            label: 'Comparar',
-                            icon: 'pi pi-briefcase',
-                            routerLink: '/plans/all'
-                        },
-                        {
-                            label: 'Acerca de',
-                            icon: 'pi pi-users',
-                            routerLink: '/plans/about'
-                        },
-                        {
-                            label: 'Compañias',
-                            icon: 'pi pi-briefcase',
-                            routerLink: '/plans/company'
-                        },
-                        {
-                            label:'Mapa',
-                            icon: 'pi pi-map',
-                            items: [
-                                {
-                                    label: 'Buscar',
-                                    icon : 'pi pi-search',
-                                    routerLink:'map/map'
-                                },
-                                {
-                                    label: 'Listado de Sucursales',
-                                    icon : 'pi pi-list',
-                                    routerLink: 'map/sucursal'
-                                }
-                            ]
-                            
-                        },
-                        // ...( this.userLoggedIn ? [{
-                        //     label:'logout',
-                        //     icon: 'pi pi-map',
-                        //     routerLink:'map/map'
-                        // }]: []),
-                        // {
-                        //     label: 'Excel',
-                        //     icon: 'pi pi-file',
-                        //     items: [
-                        //         {
-                        //             label: 'Internet',
-                        //             icon: 'pi pi-bars',
-                        //             routerLink: '/excel/internet'
-                        //         }
-                        //     ]
-                        // },
-                    ];
-                }
-            )
-    
-        
+      this.items = [
+        {
+          label: 'Planes',
+          icon: 'pi pi-comments',
+          items: [
+            {
+              label: 'Ver planes',
+              icon: 'pi pi-bars',
+              routerLink: '/plans/all',
+            },
+            ...(this.userLoggedIn
+              ? [
+                  {
+                    label: 'Agregar',
+                    icon: 'pi pi-plus',
+                    routerLink: '/plans/add',
+                  },
+                ]
+              : []),
+          ],
+        },
+        {
+          label: 'Comparar',
+          icon: 'pi pi-briefcase',
+          routerLink: '/plans/all',
+        },
+        {
+          label: 'Acerca de',
+          icon: 'pi pi-users',
+          routerLink: '/plans/about',
+        },
+        {
+          label: 'Compañias',
+          icon: 'pi pi-briefcase',
+          routerLink: '/plans/company',
+        },
+        {
+          label: 'Mapa',
+          icon: 'pi pi-map',
+          items: [
+            {
+              label: 'Buscar',
+              icon: 'pi pi-search',
+              routerLink: 'map/map',
+            },
+            {
+              label: 'Listado de Sucursales',
+              icon: 'pi pi-list',
+              routerLink: 'map/sucursal',
+            },
+          ],
+        },
+        // ...( this.userLoggedIn ? [{
+        //     label:'logout',
+        //     icon: 'pi pi-map',
+        //     routerLink:'map/map'
+        // }]: []),
+        // {
+        //     label: 'Excel',
+        //     icon: 'pi pi-file',
+        //     items: [
+        //         {
+        //             label: 'Internet',
+        //             icon: 'pi pi-bars',
+        //             routerLink: '/excel/internet'
+        //         }
+        //     ]
+        // },
+      ];
+    });
   }
 
-  logout(){
+  logout() {
     this.authService.logout();
-    this.router.navigate(['/auth/auth'])
+    this.router.navigate(['/auth/auth']);
   }
 
-  login(){
-    this.router.navigate(['/auth/auth'])
+  login() {
+    this.router.navigate(['/auth/auth']);
   }
-
 }
