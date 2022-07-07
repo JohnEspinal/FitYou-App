0b0b5ea04c7e60d29ac9826a7951029501947f14
Victor Jose Calderon <vicjocaso@gmail.com>
2022-07-06 21:14:05 -0400

    update endpoint

--- src/app/excelplans/service/service-internet.service.ts
+++ src/app/excelplans/service/service-internet.service.ts
@@ -2,24 +2,21 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class ServiceInternetService {
+  baseUrl: string = 'https://fityoubackend.azurewebsites.net/api';
 
-  filetoUpload : File;
+  filetoUpload: File;
 
-  constructor(
-    private http : HttpClient
-  ) { }
+  constructor(private http: HttpClient) {}
 
-  ImportDataFromExcel(file : File){
-    console.log("file");
+  ImportDataFromExcel(file: File) {
+    console.log('file');
     console.log(file);
     let fomdata = new FormData();
-    fomdata.append("file",file,file.name);
+    fomdata.append('file', file, file.name);
     console.log(fomdata);
-    return this.http.post(`https://localhost:44384/api/office/Importar`, fomdata);
+    return this.http.post(`${this.baseUrl}/office/Importar`, fomdata);
   }
-
 }
--- src/app/map/services/place.service.ts
+++ src/app/map/services/place.service.ts
@@ -1,4 +1,3 @@
-
 import { Injectable } from '@angular/core';
 import { MapService } from './map.service';
 import { placesApiClient } from '../api';
@@ -7,52 +6,53 @@ import { HttpClient } from '@angular/common/http';
 import { Sucursales } from '../interfaces/sucursales.interface';
 import { Observable } from 'rxjs';
 import { Company } from '../../plans/interfaces/plan.interface';
+import { MessageService } from 'primeng/api';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PlacesService {
+  baseUrl: string = 'https://fityoubackend.azurewebsites.net/api';
 
-  public userLocation? : [number, number];
-  public isLoadingPlaces : boolean = false;
-  public places : Feature[] = [];
+  public userLocation?: [number, number];
+  public isLoadingPlaces: boolean = false;
+  public places: Feature[] = [];
 
-  get iduserLocationReady() : boolean{
+  get iduserLocationReady(): boolean {
     return !!this.userLocation;
   }
 
-  constructor( 
-    private placesApi : placesApiClient ,
-    private mapservice : MapService,
-    private http : HttpClient
-    ) { 
+  constructor(
+    private placesApi: placesApiClient,
+    private mapservice: MapService,
+    private http: HttpClient,
+    private MessageService: MessageService
+  ) {
     this.getUserLocation();
   }
 
-
-  public async getUserLocation() : Promise<[number,number]>{
-
-    return new Promise( (resolve , reject) => {
-
-      navigator.geolocation.getCurrentPosition( ( {coords} ) => {
-        this.userLocation = [ coords.longitude , coords.latitude ];
-        resolve( this.userLocation );
-      },
-      error => {
-        alert("No se pudo obtener la geolocalizacion");
-        console.log(error);
-        reject();
-      });
-
-    } )
-
-
+  public async getUserLocation(): Promise<[number, number]> {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(
+        ({ coords }) => {
+          this.userLocation = [coords.longitude, coords.latitude];
+          resolve(this.userLocation);
+        },
+        (error) => {
+          this.MessageService.add({
+            severity: 'success',
+            summary: 'Ticket mensaje',
+            detail: 'Tu Ticket fue creado exitosamente',
+          });
+          console.log(error);
+          reject();
+        }
+      );
+    });
   }
 
-  getplacesbyQuery( query : string = '' ){
-    
-
-    if(query.length === 0){
+  getplacesbyQuery(query: string = '') {
+    if (query.length === 0) {
       this.isLoadingPlaces === false;
       this.places = [];
       return;
@@ -60,37 +60,40 @@ export class PlacesService {
 
     this.isLoadingPlaces = true;
 
-    if(!this.userLocation) throw new Error("No hay userlocation");
+    if (!this.userLocation) throw new Error('No hay userlocation');
 
-    this.placesApi.get<PlacesResponse>(`/${query}.json`, { 
-      params : {
-        proximity : this.userLocation.join(",")
-      }
-    }).subscribe( resp => {
+    this.placesApi
+      .get<PlacesResponse>(`/${query}.json`, {
+        params: {
+          proximity: this.userLocation.join(','),
+        },
+      })
+      .subscribe((resp) => {
         console.log(resp);
         this.isLoadingPlaces = false;
         this.places = resp.features;
-        this.mapservice.createMarkersfromplaces(this.places,this.userLocation!);
-    });
-        
+        this.mapservice.createMarkersfromplaces(
+          this.places,
+          this.userLocation!
+        );
+      });
   }
 
-  deletePlaces(){
+  deletePlaces() {
     this.places = [];
   }
 
-  getSucursales() : Observable<Sucursales[]> {
-    return this.http.get<Sucursales[]>("https://localhost:44384/api/GetOffices");
+  getSucursales(): Observable<Sucursales[]> {
+    return this.http.get<Sucursales[]>(`${this.baseUrl}/GetOffices`);
   }
 
-  getCompanyByID(id : number) : Observable<Company[]>{
-    return this.http.get<Company[]>(`https://localhost:44384/api/getCompanyById/${id}`);
+  getCompanyByID(id: number): Observable<Company[]> {
+    return this.http.get<Company[]>(`${this.baseUrl}/getCompanyById/${id}`);
   }
 
   // getPlacesByQuery(query : string){
   //   this.http.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?country=do&proximity=-70.61676567160748%2C19.029725132227398&language=es%2Cen&access_token=pk.eyJ1Ijoid2FsYXNlIiwiYSI6ImNrenBycXV6ZjBkbW4ydm9heGZrd2E2eWwifQ.96ILgOttK0FZpDYURkGsrQ`)
   //                 .subscribe(console.log);
-    
-  // }
 
+  // }
 }
